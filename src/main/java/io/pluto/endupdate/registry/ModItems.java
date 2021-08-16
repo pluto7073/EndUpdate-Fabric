42065383edcab4d839fb25fdc6337cce57109aa7
Pluto7073 <20planetpluto05@gmail.com>
2021-08-16 15:25:18 -0400

    Commented out some things.

--- src/main/java/io/pluto/endupdate/registry/ModItems.java
+++ src/main/java/io/pluto/endupdate/registry/ModItems.java
@@ -15,24 +15,41 @@ public class ModItems {
 
     public static final String MOD_ID = EndUpdate.MOD_ID;
 
+    //Other
     public static final Item CHORUS_GEM = new Item(new Item.Settings().group(ItemGroup.MATERIALS));
 
-    public static final SwordItem CHORUS_GEM_SWORD = new SwordItem(ChorusGemToolMaterial.INSTANCE, 2, 2.5f, new Item.Settings().group(ItemGroup.COMBAT));
-    public static final AxeItem CHORUS_GEM_AXE = new ChorusGemAxe(ChorusGemToolMaterial.INSTANCE, 2.5f, 2.5f, new Item.Settings().group(ItemGroup.TOOLS));
-    public static final PickaxeItem CHORUS_GEM_PICKAXE = new ChorusGemPickaxe(ChorusGemToolMaterial.INSTANCE, 1, 2.5f, new Item.Settings().group(ItemGroup.TOOLS));
-    public static final ShovelItem CHORUS_GEM_SHOVEL = new ShovelItem(ChorusGemToolMaterial.INSTANCE, 1, 2.5f, new Item.Settings().group(ItemGroup.TOOLS));
-    public static final HoeItem CHORUS_GEM_HOE = new ChorusGemHoe(ChorusGemToolMaterial.INSTANCE, 1, 2.5f, new Item.Settings().group(ItemGroup.TOOLS));
+    //Tools
+    public static final SwordItem CHORUS_GEM_SWORD = new SwordItem(ChorusGemToolMaterial.INSTANCE, 3, -2.4f, new Item.Settings().group(ItemGroup.COMBAT));
+    public static final AxeItem CHORUS_GEM_AXE = new ChorusGemAxe(ChorusGemToolMaterial.INSTANCE, 5f, -3.0f, new Item.Settings().group(ItemGroup.TOOLS));
+    public static final PickaxeItem CHORUS_GEM_PICKAXE = new ChorusGemPickaxe(ChorusGemToolMaterial.INSTANCE, 1, -2.8f, new Item.Settings().group(ItemGroup.TOOLS));
+    public static final ShovelItem CHORUS_GEM_SHOVEL = new ShovelItem(ChorusGemToolMaterial.INSTANCE, 1.5f, -3.0f, new Item.Settings().group(ItemGroup.TOOLS));
+    public static final HoeItem CHORUS_GEM_HOE = new ChorusGemHoe(ChorusGemToolMaterial.INSTANCE, -4, 0, new Item.Settings().group(ItemGroup.TOOLS));
 
+    //Armour
     public static final Item CHORUS_GEM_HELMET = new ArmorItem(ChorusGemArmorMaterial.INSTANCE, EquipmentSlot.HEAD, new Item.Settings().group(ItemGroup.COMBAT));
     public static final Item CHORUS_GEM_CHESTPLATE = new ArmorItem(ChorusGemArmorMaterial.INSTANCE, EquipmentSlot.CHEST, new Item.Settings().group(ItemGroup.COMBAT));
     public static final Item CHORUS_GEM_LEGGINGS = new ArmorItem(ChorusGemArmorMaterial.INSTANCE, EquipmentSlot.LEGS, new Item.Settings().group(ItemGroup.COMBAT));
     public static final Item CHORUS_GEM_BOOTS = new ArmorItem(ChorusGemArmorMaterial.INSTANCE, EquipmentSlot.FEET, new Item.Settings().group(ItemGroup.COMBAT));
 
-    public static Item PURPLESTONE;
-    public static Item CHORUS_BUSH;
-    public static Item CHORUS_SAPLING;
-    public static Item PURPUR_GRASS;
-    public static Item CHORUS_WOOD;
+    //BlockItems
+    public static final Item PURPLESTONE;
+    public static final Item CHORUS_BUSH;
+    public static final Item PURPUR_GRASS;
+    public static final Item CHORUS_WOOD;
+    //public static final Item ELYTRIAN_GRASS;
+
+    static {
+        /*ELYTRIAN_GRASS = Registry.register(Registry.ITEM, new Identifier(MOD_ID, "elytrian_grass"),
+                new BlockItem(ModBlocks.ELYTRIAN_GRASS, new Item.Settings().group(ItemGroup.BUILDING_BLOCKS)));*/
+        PURPLESTONE = Registry.register(Registry.ITEM, new Identifier(MOD_ID, "purplestone"),
+                new BlockItem(ModBlocks.PURPLESTONE, new Item.Settings().group(ItemGroup.BUILDING_BLOCKS)));
+        CHORUS_BUSH = Registry.register(Registry.ITEM, new Identifier(MOD_ID, "chorus_bush"),
+                new BlockItem(ModBlocks.CHORUS_BUSH, new Item.Settings().group(ItemGroup.DECORATIONS)));
+        PURPUR_GRASS = Registry.register(Registry.ITEM, new Identifier(MOD_ID, "purpur_grass"),
+                new BlockItem(ModBlocks.PURPUR_GRASS, new Item.Settings().group(ItemGroup.BUILDING_BLOCKS)));
+        CHORUS_WOOD = Registry.register(Registry.ITEM, new Identifier(MOD_ID, "chorus_wood"),
+                new BlockItem(ModBlocks.CHORUS_WOOD, new Item.Settings().group(ItemGroup.BUILDING_BLOCKS)));
+    }
 
     public static void register() {
         Registry.register(Registry.ITEM, new Identifier(MOD_ID, "chorus_gem"), CHORUS_GEM);
@@ -47,17 +64,6 @@ public class ModItems {
         Registry.register(Registry.ITEM, new Identifier(MOD_ID, "chorus_gem_chestplate"), CHORUS_GEM_CHESTPLATE);
         Registry.register(Registry.ITEM, new Identifier(MOD_ID, "chorus_gem_leggings"), CHORUS_GEM_LEGGINGS);
         Registry.register(Registry.ITEM, new Identifier(MOD_ID, "chorus_gem_boots"), CHORUS_GEM_BOOTS);
-
-        PURPLESTONE = Registry.register(Registry.ITEM, new Identifier(MOD_ID, "purplestone"),
-                new BlockItem(ModBlocks.PURPLESTONE, new Item.Settings().group(ItemGroup.BUILDING_BLOCKS)));
-        CHORUS_BUSH = Registry.register(Registry.ITEM, new Identifier(MOD_ID, "chorus_bush"),
-                new BlockItem(ModBlocks.CHORUS_BUSH, new Item.Settings().group(ItemGroup.DECORATIONS)));
-        CHORUS_SAPLING = Registry.register(Registry.ITEM, new Identifier(MOD_ID, "chorus_sapling"),
-                new BlockItem(ModBlocks.CHORUS_SAPLING, new Item.Settings().group(ItemGroup.DECORATIONS)));
-        PURPUR_GRASS = Registry.register(Registry.ITEM, new Identifier(MOD_ID, "purpur_grass"),
-                new BlockItem(ModBlocks.PURPUR_GRASS, new Item.Settings().group(ItemGroup.BUILDING_BLOCKS)));
-        CHORUS_WOOD = Registry.register(Registry.ITEM, new Identifier(MOD_ID, "chorus_wood"),
-                new BlockItem(ModBlocks.CHORUS_WOOD, new Item.Settings().group(ItemGroup.BUILDING_BLOCKS)));
     }
 
 }
